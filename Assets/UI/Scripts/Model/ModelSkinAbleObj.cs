27910898dcde13d41051e2c114f5dccbaffbb24c
Keyzi <Keyzi2002@gmail.com>
2022-09-19 20:19:10 +0700

    destroy

--- Assets/UI/Scripts/Model/ModelSkinAbleObj.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "Model Skin", menuName = "ScriptAbleObjects/New Model Skin")]
-public class ModelSkinAbleObj : ModelAbleObj
-{
-    public NameColor color;
-}
